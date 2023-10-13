7eee8d9d83051e7b1cb008489236f3d25b05b2c0
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-13 11:25:46 -0600

    Object.getOwnPropertyDescriptors

--- pract/main.js
+++ pract/main.js
@@ -2,13 +2,17 @@ const juan = {
     name: "Juanito",
     age: 18,
     approvedCourses: ["Curso 1"],
-    addCourse() {
+    addCourse(newCourse) {
+        console.log("This", this);
+        console.log("This.approvedCourses", this.approvedCourses);
         this.approvedCourses.push(newCourse);
     }
 };
 
-console.log(
-Object.keys(juan), // Este método estático devuelve un array de las propiedades enumerables de un objeto.
-Object.getOwnPropertyNames(juan), // Este método devuelve un array con todas las propiedades (tanto enumerables como no enumerables) de un objeto, excluyendo los símbolos.
-Object.entries(juan), // Devuelve un array de arrays, donde cada subarray contiene una pareja clave-valor correspondiente a las propiedades enumerables del objeto.
-);
+// console.log(
+// Object.keys(juan), // Este método estático devuelve un array de las propiedades enumerables de un objeto.
+// Object.getOwnPropertyNames(juan), // Este método devuelve un array con todas las propiedades (tanto enumerables como no enumerables) de un objeto, excluyendo los símbolos.
+// Object.entries(juan), // Devuelve un array de arrays, donde cada subarray contiene una pareja clave-valor correspondiente a las propiedades enumerables del objeto.
+// );
+
+console.log(Object.getOwnPropertyDescriptors(juan));
