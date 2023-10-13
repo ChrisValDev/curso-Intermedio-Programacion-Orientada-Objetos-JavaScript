637a27df5eeb34b36431c014a06b05974d305244
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-13 15:19:02 -0600

    Ejemplo de extraccion de elementos de una lista

--- pract/main.js
+++ pract/main.js
@@ -1,11 +1,8 @@
+//Ejemplo de condicional para extraer datos de una lista(array)
 
-
-//Recursividad
-//Ejemplo
-function recursiva() {
-    if(/*validacion*/) {
-        //llamados recursivos
-    } else {
-        //llamados normales, sin recursividad
-    }
+const numeritos = [2,3,45,256,6,3,7,3,76,89,3,5,,1];
+let numerito = 0;
+for (let index = 0; index < numeritos.length; index++) {
+    numerito = numeritos[index];
+    console.log({ index, numerito });
 }
