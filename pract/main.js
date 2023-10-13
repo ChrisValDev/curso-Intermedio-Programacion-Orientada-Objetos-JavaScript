215183b30177ffb4c23b5d4bc1cf14113cc5fd89
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-13 14:32:37 -0600

    Ejemplo de Shallow copy

--- pract/main.js
+++ pract/main.js
@@ -1,31 +1,19 @@
-const juan = {
-    name: "Juanito",
-    age: 18,
-    approvedCourses: ["Curso 1"],
-    addCourse(newCourse) {
-        this.approvedCourses.push(newCourse);
-    }
+//Shallow copy
+const obj1 = {
+    a: "a",
+    b: "b",
+    c: {
+        d: "d",
+        e: "e",
+    },
 };
 
+const obj2 = {};
 
-Object.defineProperty(juan, "navigator", {
-    value: "Chrome",
-    enumerable: true,
-    writable: true,
-    configurable: true,
-});
+for (prop in obj1) {
+    obj2[prop] = obj1[prop];
+}
 
+const obj3 = Object.assign({}, obj1);
 
-//Podemos decir que freeze y seal modifican "descriptores de propiedad" para las propiedades del objeto completo.
-
-// Object.freeze(objeto);
-
-// enumerable: true,
-// writable: false,
-// configurable: false,
-
-// Object.seal(objeto);
-
-// enumerable: true,
-// writable: true,
-// configurable: false,
+// Una "Shallow copy" (copia superficial) en JavaScript se refiere a la creación de un nuevo objeto o estructura de datos que es una copia de otro objeto, pero donde solo se duplica la primera capa de propiedades o elementos. Las propiedades u elementos anidados como en el ejemplo anterior son las propiedades d: "d" y e: "e", estas no se copian en profundidad(solo superficialmente), por ello se mantienen como referencias al objeto original. Lo que ocasiona que si modificamos alguna de estas propiedades (d,e) anidadas en el Obj1 se haran los mismos cambios para Obj3 y viceversa.
