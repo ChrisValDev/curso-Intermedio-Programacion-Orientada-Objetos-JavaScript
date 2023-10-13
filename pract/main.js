bac7faddef5aeaea6adf7c1b62a5ee61769c4be3
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-13 15:31:19 -0600

    Ejemplo de extraccion de elementos de una lista de manera recursiva

--- pract/main.js
+++ pract/main.js
@@ -1,8 +1,13 @@
 //Ejemplo de condicional para extraer datos de una lista(array)
 
-const numeritos = [2,3,45,256,6,3,7,3,76,89,3,5,,1];
-let numerito = 0;
-for (let index = 0; index < numeritos.length; index++) {
-    numerito = numeritos[index];
-    console.log({ index, numerito });
+const numeritos = [2,3,45,256,6,3,7,3,76,89,3,5,1];
+
+function recursiva(numbersArray) {
+    if(numbersArray.length != 0) {
+        const firstNum = numbersArray[0];
+        console.log(firstNum);
+
+        numbersArray.shift();
+        recursiva(numbersArray);
+    }
 }
