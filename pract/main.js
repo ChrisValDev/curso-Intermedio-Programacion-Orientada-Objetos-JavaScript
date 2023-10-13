01d4e50e3190cb7b8dd61944e8b8fc60844114c4
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-13 11:37:55 -0600

    Object.defineProperty

--- pract/main.js
+++ pract/main.js
@@ -3,16 +3,14 @@ const juan = {
     age: 18,
     approvedCourses: ["Curso 1"],
     addCourse(newCourse) {
-        console.log("This", this);
-        console.log("This.approvedCourses", this.approvedCourses);
         this.approvedCourses.push(newCourse);
     }
 };
 
-// console.log(
-// Object.keys(juan), // Este método estático devuelve un array de las propiedades enumerables de un objeto.
-// Object.getOwnPropertyNames(juan), // Este método devuelve un array con todas las propiedades (tanto enumerables como no enumerables) de un objeto, excluyendo los símbolos.
-// Object.entries(juan), // Devuelve un array de arrays, donde cada subarray contiene una pareja clave-valor correspondiente a las propiedades enumerables del objeto.
-// );
+console.log(Object.getOwnPropertyDescriptors(juan)); // Este método devuelve un objeto con descripciones de todas las propiedades (enumerables y no enumerables) de un objeto, incluyendo sus configuraciones (como si son configurables, enumerables y tienen acceso de escritura).
 
-console.log(Object.getOwnPropertyDescriptors(juan));
+Object.defineProperty(juan, "pruebaNasa", { // Método que se utiliza para definir o modificar las propiedades de un objeto en JavaScript. Específicamente, te permite configurar propiedades de un objeto, incluyendo su valor, enumerabilidad, capacidad de escritura y más.
+    value: "extraterrestres",
+    enumerable: true,
+    writable: true,
+});
